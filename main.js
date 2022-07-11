f9b75e2742b9da8efb218c98a33cb6c3e02d4009
uduak <lorduduak@gmail.com>
2022-07-11 21:46:14 +0100

    contact btn floating on mobile

--- main.js
+++ main.js
@@ -1,21 +1,28 @@
 // get footer date automatically
 const date = document.querySelector(".date");
 date.textContent = new Date().getFullYear();
-const menuBtn = document.querySelector('.menu-drop');
+const menuBtn = document.querySelector(".menu-drop");
 
 // make navbar stick on scroll
+const navContact = document.querySelector(".nav-contact");
+
+// hide and show nav contact on scroll
+const display = window.getComputedStyle(navContact).display;
+let navShow;
+
+if (display == 'flex') {
+  navShow = true;
+} else {
+  navShow = false;
+}
 
 window.addEventListener("scroll", function () {
   const navItems = document.querySelector("#nav");
-//   const nav = document.querySelector(".nav");
   const scrollHeight = window.pageYOffset; //pageYOffset returns the current scroll position
-//   const navHeight = navItems.getBoundingClientRect().height; //returns the height of the item
   const navLink = document.querySelectorAll(".nav-link");
 
   // make back-to-top button appear on scroll
   const topBtn = document.querySelector(".top-btn");
-
-  const navContact = document.querySelector(".nav-contact");
 
   if (scrollHeight > 200) {
     topBtn.classList.add("btn-show");
@@ -25,17 +32,21 @@ window.addEventListener("scroll", function () {
 
   // sticky nav
 
-  let navAddress = false;
+  if (navShow == "true") {
+    navShow = true;
+  } else {
+    navShow = false;
+  }
 
-  if (scrollHeight > 100) {
+  if (scrollHeight > 300) {
     navItems.classList.add("nav-sticky");
     menuBtn.style.color = "var(--primary)";
-    
-    const display = window.getComputedStyle(navContact).display;
+
+    // const display = window.getComputedStyle(navContact).display;
 
     if (display != "none") {
-        navAddress = true;
-        navContact.style.display = 'none';
+      // navContact.style.display = "none";
+      navContact.style.display = 'none';
     }
 
     //changes the color of the nav links on scroll
@@ -45,10 +56,10 @@ window.addEventListener("scroll", function () {
       }
     });
   } else {
-
-    if (navAddress == true) {
-        navContact.style.display = "flex";
-        navAddress = false;
+    if (display != "none") {
+      navContact.style.display = "flex";
+    } else {
+      navContact.style.display = "none"
     }
 
     navItems.classList.remove("nav-sticky");
@@ -58,3 +69,24 @@ window.addEventListener("scroll", function () {
     });
   }
 });
+
+
+// contact button on mobile
+let contactBtn = document.querySelector('.con-btn');
+let contactInfo = document.querySelector('.contact-popup-info');
+let cancelBtn = contactInfo.querySelector('.cancel');
+
+contactBtn.addEventListener('click', ()=>{
+  contactInfo.style.display = "block";
+  contactInfo.animate([
+    {opacity: 0}, {opacity: 1}
+  ], 
+  {
+    duration: 400,
+  })
+  
+})
+
+cancelBtn.addEventListener('click', ()=>{
+  contactInfo.style.display = "none";
+})
